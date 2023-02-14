b69003d588e3932d685612e9df0524e3d619158a
SergeyDedikov <serded59@yandex.ru>
2023-02-14 18:34:29 +0500

    feat: plug DB

--- app/app.js
+++ app/app.js
@@ -5,6 +5,21 @@ const app = express();
 // Router
 const router = express.Router();
 
+// DB
+const { Pool } = require("pg");
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+});
+
+pool.query("SELECT NOW()", (err, res) => {
+    console.log(err, res);
+    pool.end();
+});
+
 // Главная
 router.get("/", (_req, res) => {
     res.status(200).json({
