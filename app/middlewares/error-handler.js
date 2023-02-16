24b8bcbf5d51b7626a02414eb19ddc90762128f4
SergeyDedikov <serded59@yandex.ru>
2023-02-16 11:18:39 +0500

    feat: mod mw error handler

--- app/middlewares/error-handler.js
+++ app/middlewares/error-handler.js
@@ -1,8 +1,8 @@
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
+  // const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
 
-  res.status(statusCode).send({ message });
+  res.status(statusCode).send({ message: err.message });
 
   next();
 };
