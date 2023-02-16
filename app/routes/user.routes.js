411ac7763c1587bbf44466ee101743e939af51d0
SergeyDedikov <serded59@yandex.ru>
2023-02-16 11:15:44 +0500

    feat: create users routes

---
+++ app/routes/user.routes.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+
+const { authJwt } = require("../middlewares");
+const controller = require("../controllers/user.controller");
+
+router.use((req, res, next) => {
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    next();
+});
+
+router.get("/all", controller.allAccess);
+
+router.get("/user", [authJwt.verifyToken], controller.userBoard);
+
+router.get(
+    "/mod",
+    [authJwt.verifyToken, authJwt.isModerator],
+    controller.moderatorBoard
+);
+
+router.get(
+    "/admin",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.adminBoard
+);
+
+module.exports = router;
